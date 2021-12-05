0b561e4520ff07d78fc033126d4d0035f20cd809
tdvenky <31875310+tdvenky@users.noreply.github.com>
2021-12-05 15:05:53 -0500

    created a maven java project and added a class for the first kata

---
+++ src/test/java/com/katas/tdd/NumberToWordsTest.java
@@ -0,0 +1,9 @@
+package com.katas.tdd;
+
+import com.katas.tdd.NumberToWords;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+
+public class NumberToWordsTest extends TestCase {
+}
