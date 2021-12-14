7a241b6e973f88d9ebf54790fe34d2d015e8db78
tdvenky <31875310+tdvenky@users.noreply.github.com>
2021-12-14 03:51:22 -0500

    updating Junit version to 5

--- src/test/java/com/katas/tdd/NumberToWordsTest.java
+++ src/test/java/com/katas/tdd/NumberToWordsTest.java
@@ -1,9 +1,4 @@
 package com.katas.tdd;
 
-import com.katas.tdd.NumberToWords;
-
-import junit.framework.Test;
-import junit.framework.TestCase;
-
-public class NumberToWordsTest extends TestCase {
+public class NumberToWordsTest {
 }
