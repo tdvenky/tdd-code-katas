f93a5af6635c69d4373d8d095f241746c80109d7
tdvenky <31875310+tdvenky@users.noreply.github.com>
2021-12-14 04:28:09 -0500

    found duplicate code, refactoring to eliminate it

--- src/main/java/com/katas/tdd/NumberToWords.java
+++ src/main/java/com/katas/tdd/NumberToWords.java
@@ -1,17 +1,26 @@
 package com.katas.tdd;
 
+import java.util.HashMap;
+
 public class NumberToWords {
+    private HashMap<Integer, String> singleDigitNumberToWords;
+
+    private void initSingleDigitNumberToWords() {
+        singleDigitNumberToWords = new HashMap<Integer,String>();
+
+        singleDigitNumberToWords.put(0, "zero");
+        singleDigitNumberToWords.put(1, "one");
+    }
 
 	public String convertNumberToWords(Integer number) {
         StringBuilder words = new StringBuilder();
 
         if(number == null) return words.toString();
 
-        if(number == 0) {
-            words.append("zero");
-        } else if(number == 1) {
-            words.append("one");
-        }
+        initSingleDigitNumberToWords();
+
+        words.append(singleDigitNumberToWords.get(number));
+
 		return words.toString();
 	}
 }
