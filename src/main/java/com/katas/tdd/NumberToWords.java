e4c823a5716c91d506d9d97bd0c82e6ff3d9d400
tdvenky <31875310+tdvenky@users.noreply.github.com>
2021-12-14 04:10:09 -0500

    adding first test for empty/null input

--- src/main/java/com/katas/tdd/NumberToWords.java
+++ src/main/java/com/katas/tdd/NumberToWords.java
@@ -1,4 +1,8 @@
 package com.katas.tdd;
 
 public class NumberToWords {
+
+	public String convertNumberToWords(Integer number) {
+		return "";
+	}
 }
--- src/test/java/com/katas/tdd/NumberToWordsTest.java
+++ src/test/java/com/katas/tdd/NumberToWordsTest.java
@@ -1,4 +1,26 @@
 package com.katas.tdd;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class NumberToWordsTest {
+    NumberToWords numberToWords;
+
+    @BeforeEach
+    public void init() {
+        numberToWords = new NumberToWords();
+    }
+
+    @AfterEach
+    public void destroy() {
+        numberToWords = null;
+    }
+
+    @Test
+    public void emptyInputNumber_ReturnsEmptyString() {
+        assertEquals("", numberToWords.convertNumberToWords(null));
+    }
 }
